136169cce870b85a418919fc4c226110714b24ad
pavoine <pavoine>
2004-08-31 13:20:15 +0000

    Ajout d'un paramètre

--- R/divc.R
+++ R/divc.R
@@ -1,4 +1,4 @@
-divc <- function(df, dis = NULL){
+divc <- function(df, dis = NULL, scale = FALSE){
     # checking of user's data and initialization.
     if (!inherits(df, "data.frame")) stop("Non convenient df")
     if (any(df < 0)) stop("Negative value in df")
@@ -7,13 +7,19 @@ divc <- function(df, dis = NULL){
         if (!is.euclid(dis)) stop("Euclidean property is expected for distance")
         dis <- as.matrix(dis)
         if (nrow(df)!= nrow(dis)) stop("Non convenient df")
+        dis <- as.dist(dis)
     }
-    if (is.null(dis)) dis <- (matrix(1, nrow(df), nrow(df)) - diag(rep(1, nrow(df)))) * sqrt(2)
+    if (is.null(dis)) dis <- as.dist((matrix(1, nrow(df), nrow(df))
+        - diag(rep(1, nrow(df)))) * sqrt(2))
     div <- as.data.frame(rep(0, ncol(df)))
     names(div) <- "diversity"
     rownames(div) <- names(df)
     for (i in 1:ncol(df)) {
         div[i, ] <- (t(df[, i]) %*% (as.matrix(dis)^2) %*% df[, i]) / 2 / (sum(df[, i])^2)
+    }
+    if(scale == TRUE){
+        divmax <- divcmax(dis)$value
+        div <- div / divmax
     }
     return(div)
 }

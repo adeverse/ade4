519d8e39783b7d335ec77a441f7b6aa12631a75d
chessel <chessel>
2006-07-04 07:12:08 +0000

    *** empty log message ***

--- R/reconst.R
+++ R/reconst.R
@@ -26,3 +26,26 @@
     row.names(res) <- row.names(dudi$tab)
     return(res)
 }
+
+"reconst.coa" <- function (dudi, nf = 1, ...) {
+    if (!inherits(dudi, "dudi")) 
+        stop("Object of class 'dudi' expected")
+    if (nf > dudi$nf) 
+        stop(paste(nf, "factors need >", dudi$nf, "factors available\n"))
+    if (!inherits(dudi, "coa")) 
+        stop("Object of class 'dudi' expected")
+    pl <- dudi$lw
+    pc <- dudi$cw
+    n <- dudi$N
+    res0 <- outer(pl,pc)*n
+    res <- data.frame(res0)
+    names(res) <- names(dudi$tab)
+    row.names(res) <- row.names(dudi$tab)
+    if (nf ==0) return(res)
+    for (i in 1:nf) {
+        xli <- dudi$li[, i]
+        yc1 <- dudi$c1[, i]
+        res <- res + outer(xli,yc1)*res0
+    }
+    return(res)
+}

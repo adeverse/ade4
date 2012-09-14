b700109bc6272532f65a5d68b91af855ee238377
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2012-09-14 11:19:00 +0000

    Calls to Fortran routine 'dqrls' are replaced by public 'lm.wfit'

--- R/randtest.cca.R
+++ R/randtest.cca.R
@@ -4,33 +4,25 @@
     if (!inherits(xtest, "pcaiv")) 
         stop("Type 'cca' expected")
     appel <- as.list(xtest$call)
-    df <- eval.parent(appel$sitenv)
+    df <- as.data.frame(eval.parent(appel$sitenv))
     spe <- eval.parent(appel$sitspe)
     coa1 <- dudi.coa(spe, scannf = FALSE)
     y <- as.matrix(coa1$tab)
     sqlw <- sqrt(coa1$lw)
     sqcw <- sqrt(coa1$cw)
     inertot <- sum(coa1$eig)
-    ## Fast function for computing sum of squares of the fitted table from routines 'dqrls{base}'. 
-    lmwfit <- function(y,x,sqlw,sqcw) {
-      n <- nrow(x)
-      p <- ncol(x)
-      ny <- NCOL(y)
-      storage.mode(y) <- "double"
-      z <- .Fortran("dqrls",qr=x*sqlw,n=n,p=p,y=y*sqlw,ny=ny,tol=1e-07,coefficients=mat.or.vec(p,ny),residuals=y,effects=mat.or.vec(n,ny),rank =integer(1),pivot=1:p,qraux=double(p),work=double(2*p), PACKAGE="base")
-      tab <- sqlw*y-z$residuals
-      tab <- sweep(tab,2,sqcw,"*")
-      return(sum(tab^2))
-    }
     
     fmla <- as.formula(paste("y ~", paste(dimnames(df)[[2]], collapse = "+")))
     mf <- model.frame(fmla,data=cbind.data.frame(y,df))
     mt <- attr(mf,"terms")
     x <- model.matrix(mt,mf)
-    obs <- lmwfit(y,x,sqlw,sqcw)/inertot
+    wt <- outer(sqlw, sqcw)
+    ## Fast function for computing sum of squares of the fitted table 
+    obs <- sum((lm.wfit(y = y,x = x, w = coa1$lw)$fitted.values * wt)^2) / inertot
+        
     isim <- c()
     for(i in 1:nrepet)
-      isim[i] <- lmwfit(y,x[sample(nrow(x)),],sqlw,sqcw)/inertot
+      isim[i] <- sum((lm.wfit(y = y,x = x[sample(nrow(x)),], w = coa1$lw)$fitted.values * wt)^2) / inertot
     return(as.randtest(isim,obs,call=match.call()))
     
  }

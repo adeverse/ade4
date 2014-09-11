7dff8ae11dd4cb0da2ddd754bb177175b5a6f0a7
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2014-09-11 14:50:54 +0000

    Change names of arguments and outputs of 'procuste' function and add a method 'randtest' for procuste class

--- R/procuste.R
+++ R/procuste.R
@@ -1,27 +1,23 @@
-"procuste" <- function (df1, df2, scale = TRUE, nf = 4, tol = 1e-07) {
-    df1 <- data.frame(df1)
-    df2 <- data.frame(df2)
-    if (!is.data.frame(df1)) 
+"procuste" <- function (dfX, dfY, scale = TRUE, nf = 4, tol = 1e-07) {
+    dfX <- data.frame(dfX)
+    dfY <- data.frame(dfY)
+    if (!is.data.frame(dfX)) 
         stop("data.frame expected")
-    if (!is.data.frame(df2)) 
+    if (!is.data.frame(dfY)) 
         stop("data.frame expected")
-    l1 <- nrow(df1)
-    if (nrow(df2) != l1) 
+    if (nrow(dfY) != nrow(dfX)) 
         stop("Row numbers are different")
-    if (any(row.names(df2) != row.names(df1))) 
+    if (any(row.names(dfY) != row.names(dfX))) 
         stop("row names are different")
-    c1 <- ncol(df1)
-    c2 <- ncol(df2)
-    X <- scale(df1, scale = FALSE)
-    Y <- scale(df2, scale = FALSE)
-    var1 <- apply(X, 2, function(x) sum(x^2))
-    var2 <- apply(Y, 2, function(x) sum(x^2))
-    tra1 <- sum(var1)
-    tra2 <- sum(var2)
+    
+    X <- scale(dfX, scale = FALSE)
+    Y <- scale(dfY, scale = FALSE)
+
     if (scale) {
-        X <- X/sqrt(tra1)
-        Y <- Y/sqrt(tra2)
+        X <- X/sqrt(sum(apply(X, 2, function(x) sum(x^2))))
+        Y <- Y/sqrt(sum(apply(Y, 2, function(x) sum(x^2))))
     }
+    
     X <-as.matrix(X)
     Y <- as.matrix(Y)
     PS <- t(X) %*% Y
@@ -31,48 +27,48 @@
         nf <- rank
     u <- svd1$u[, 1:nf]
     v <- svd1$v[, 1:nf]
-    scor1 <- X %*% u
-    scor2 <- Y %*% v
-    rot1 <- X %*% u %*% t(v)
-    rot2 <- Y %*% v %*% t(u)
+    scorX <- X %*% u
+    scorY <- Y %*% v
+    rotX <- X %*% u %*% t(v)
+    rotY <- Y %*% v %*% t(u)
     res <- list()
     X <- data.frame(X)
-    row.names(X) <- row.names(df1)
-    names(X) <- names(df1)
+    row.names(X) <- row.names(dfX)
+    names(X) <- names(dfX)
     Y <- data.frame(Y)
-    row.names(Y) <- row.names(df2)
-    names(Y) <- names(df2)
+    row.names(Y) <- row.names(dfY)
+    names(Y) <- names(dfY)
     res$d <- svd1$d
     res$rank <- rank
     res$nf <- nf
     u <- data.frame(u)
-    row.names(u) <- names(df1)
+    row.names(u) <- names(dfX)
     names(u) <- paste("ax", 1:nf, sep = "")
     v <- data.frame(v)
-    row.names(v) <- names(df2)
+    row.names(v) <- names(dfY)
     names(v) <- paste("ax", 1:nf, sep = "")
-    scor1 <- data.frame(scor1)
-    row.names(scor1) <- row.names(df1)
-    names(scor1) <- paste("ax", 1:nf, sep = "")
-    scor2 <- data.frame(scor2)
-    row.names(scor2) <- row.names(df1)
-    names(scor2) <- paste("ax", 1:nf, sep = "")
-    if ((nf == c1) & (nf == c2)) {
-        rot1 <- data.frame(rot1)
-        row.names(rot1) <- row.names(df1)
-        names(rot1) <- names(df2)
-        rot2 <- data.frame(rot2)
-        row.names(rot2) <- row.names(df1)
-        names(rot2) <- names(df1)
-        res$rot1 <- rot1
-        res$rot2 <- rot2
+    scorX <- data.frame(scorX)
+    row.names(scorX) <- row.names(dfX)
+    names(scorX) <- paste("ax", 1:nf, sep = "")
+    scorY <- data.frame(scorY)
+    row.names(scorY) <- row.names(dfX)
+    names(scorY) <- paste("ax", 1:nf, sep = "")
+    if ((nf ==  ncol(dfX)) & (nf == ncol(dfY))) {
+        rotX <- data.frame(rotX)
+        row.names(rotX) <- row.names(dfX)
+        names(rotX) <- names(dfY)
+        rotY <- data.frame(rotY)
+        row.names(rotY) <- row.names(dfX)
+        names(rotY) <- names(dfX)
+        res$rotX <- rotX
+        res$rotY <- rotY
     }
-    res$tab1 <- X
-    res$tab2 <- Y
-    res$load1 <- u
-    res$load2 <- v
-    res$scor1 <- scor1
-    res$scor2 <- scor2
+    res$tabX <- X
+    res$tabY <- Y
+    res$loadX <- u
+    res$loadY <- v
+    res$scorX <- scorX
+    res$scorY <- scorY
     res$call <- match.call()
     class(res) <- "procuste"
     return(res)
@@ -94,16 +90,16 @@
     layout(matrix(c(1, 2, 3, 4, 4, 5, 4, 4, 6), 3, 3), 
         respect = TRUE)
     par(mar = c(0.1, 0.1, 0.1, 0.1))
-    s.arrow(x$load1, xax, yax, sub = "Loadings 1", csub = 2, 
+    s.arrow(x$loadX, xax, yax, sub = "X loadings", csub = 2, 
         clabel = 1.25)
-    s.arrow(x$load2, xax, yax, sub = "Loadings 2", csub = 2, 
+    s.arrow(x$loadY, xax, yax, sub = "Y loadings", csub = 2, 
         clabel = 1.25)
     scatterutil.eigen(x$d^2, wsel = c(xax, yax))
-    s.match(x$scor1, x$scor2, xax, yax, clabel = 1.5, sub = "Common projection", 
+    s.match(x$scorX, x$scorY, xax, yax, clabel = 1.5, sub = "Row scores (X -> Y)", 
         csub = 2)
-    s.label(x$scor1, xax = xax, yax = yax, sub = "Array 1", 
+    s.label(x$scorX, xax = xax, yax = yax, sub = "X row scores", 
         csub = 2, clabel = 1.25)
-    s.label(x$scor2, xax = xax, yax = yax, sub = "Array 2", 
+    s.label(x$scorY, xax = xax, yax = yax, sub = "Y row scores", 
         csub = 2, clabel = 1.25)
 }
 
@@ -120,20 +116,34 @@
     if (l0 > 5) 
         cat(" ...\n")
     else cat("\n")
-    cat("tab1   data.frame  ", nrow(x$tab1), "  ", ncol(x$tab1), 
-        "   scaled array 1\n")
-    cat("tab2   data.frame  ", nrow(x$tab2), "  ", ncol(x$tab2), 
-        "   scaled array 2\n")
-    cat("scor1  data.frame  ", nrow(x$scor1), " ", ncol(x$scor1), 
-        "   row coordinates 1\n")
-    cat("scor2  data.frame  ", nrow(x$scor2), " ", ncol(x$scor2), 
-        "   row coordinates 2\n")
-    cat("load1  data.frame  ", nrow(x$load1), " ", ncol(x$load1), 
-        "   loadings 1\n")
-    cat("load2  data.frame  ", nrow(x$load2), " ", ncol(x$load2), 
-        "   loadings 2\n")
+    cat("tabX   data.frame  ", nrow(x$tabX), "  ", ncol(x$tabX), 
+        "   scaled table X\n")
+    cat("tabY   data.frame  ", nrow(x$tabY), "  ", ncol(x$tabY), 
+        "   scaled table Y\n")
+    cat("scorX  data.frame  ", nrow(x$scorX), " ", ncol(x$scorX), 
+        "   X row scores\n")
+    cat("scorY  data.frame  ", nrow(x$scorY), " ", ncol(x$scorY), 
+        "   Y row scores\n")
+    cat("loadX  data.frame  ", nrow(x$loadX), " ", ncol(x$loadX), 
+        "   X loadings\n")
+    cat("loadY  data.frame  ", nrow(x$loadY), " ", ncol(x$loadY), 
+        "   Y loadings\n")
     if (length(names(x)) > 12) {
         cat("other elements: ")
         cat(names(x)[11:(length(x))], "\n")
     }
+}
+
+
+"randtest.procuste" <- function(xtest, nrepet = 999, ...) {
+    if(!inherits(xtest,"procuste"))
+    stop("Object of class 'procuste' expected")
+
+    
+    lig <- nrow(xtest$tabX)
+    c1 <- ncol(xtest$tabX)
+    c2 <- ncol(xtest$tabY)
+    isim <- testprocuste(nrepet, lig, c1, c2, as.matrix(xtest$tabX), as.matrix(xtest$tabY))
+    obs <- isim[1]
+    return(as.randtest(isim[-1], obs, call = match.call()))
 }

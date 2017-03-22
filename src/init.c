a78feb2782a989faae2f6e4bf37177aad0ed51b8
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2017-03-22 17:37:48 +0100

    Fix an error with C routines in a Windows configuration (32bit)

--- src/init.c
+++ src/init.c
@@ -1,9 +1,7 @@
+#include <R.h>
+#include <Rinternals.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
-
-/* FIXME: 
-Check these declarations against the C/Fortran source code.
-*/
 
 /* .C calls */
 extern void gearymoran(void *, void *, void *, void *, void *, void *, void *);

6cefd2f253856e2d6af72370b8a9b6a2c60d5002
Jean Thioulouse <jean.thioulouse@univ-lyon1.fr>
2022-06-02 18:51:22 +0200

    added C_ to the Rcpp function name

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 RVrandtestCpp <- function(X, Y, nrepet) {
-    .Call(`_ade4_RVrandtestCpp`, X, Y, nrepet)
+    .Call(`C__ade4_RVrandtestCpp`, X, Y, nrepet)
 }
 

f155eb70d09c5b9c87bf825d009765abb2c06048
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2024-10-16 15:39:08 +0200

    Udpate the aspect ratio in the ggplot version of s.label

--- R/s.label.R
+++ R/s.label.R
@@ -57,8 +57,8 @@
       ggplot2::geom_vline(ggplot2::aes(xintercept = 0)) +
       ggplot2::geom_label() +
       ggplot2::theme_bw() +
-      ggplot2::theme(aspect.ratio = 1,
-                     axis.text = ggplot2::element_blank(),
+      ggplot2::coord_fixed(ratio = 1) +
+      ggplot2::theme(axis.text = ggplot2::element_blank(),
                      axis.title = ggplot2::element_blank(),
                      axis.ticks = ggplot2::element_blank())
     

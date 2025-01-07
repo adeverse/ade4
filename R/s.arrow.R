ea065ee8fa2adffaeaab64141f158e128a7df7aa
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2025-01-07 17:39:22 +0100

    Add a first version of 's.arrow' with 'ggplot2' and add a test file

--- R/s.arrow.R
+++ R/s.arrow.R
@@ -49,6 +49,21 @@
     stop("ggplot2 needed for this function to work with plotstyle = 'ggplot'. Please install it", call. = FALSE)
     
   } else {
-    return(NULL)
+    ggdfxy <- data.frame(x = dfxy[, xax], y = dfxy[, yax], lab = label)
+    colnames(ggdfxy)[1:2] <- colnames(dfxy)[c(xax, yax)]
+    
+    ggsclass <- 
+      ggplot2::ggplot(data = ggdfxy, ggplot2::aes(x = .data[[colnames(ggdfxy)[1]]],
+                                                  y = .data[[colnames(ggdfxy)[2]]],
+                                                  label = .data$lab)) +
+      ggplot2::geom_hline(ggplot2::aes(yintercept = 0)) +
+      ggplot2::geom_vline(ggplot2::aes(xintercept = 0)) +
+      ggplot2::geom_segment(aes(x = 0, y = 0, xend = .data[[colnames(ggdfxy)[1]]], yend = .data[[colnames(ggdfxy)[2]]]),
+                            arrow = ggplot2::arrow(length = ggplot2::unit(0.2, "cm"))) +
+      ggrepel::geom_label_repel() +
+      ggplot2::theme_bw() +
+      ggplot2::coord_fixed(ratio = 1)
+    
+    return(ggsclass)
   }
 }
---
+++ tests/t-s.arrow.R
@@ -0,0 +1,13 @@
+library(ade4)
+library(ggplot2)
+library(ggrepel)
+library(ggpp)
+set.seed(1234)
+
+# graphics version
+dfxy <- cbind.data.frame(runif(55,-2,3), runif(55,-3,2))
+s.arrow(dfxy)
+
+
+# ggplot version
+s.arrow(dfxy, plotstyle = "ggplot")

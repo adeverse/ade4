29d8c5a1170ab596676589c0502937f1a7a1880f
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2025-06-10 16:46:20 +0200

    Update s_gg.class: remove legend box

--- R/s_gg.class.R
+++ R/s_gg.class.R
@@ -50,7 +50,8 @@
       ggplot2::geom_label(data = dfcentroid, mapping = aes(x = .data$meanx, y = .data$meany, label = .data$label, col = .data$label), inherit.aes = FALSE) +
       ggforce::geom_ellipse(data = dfcentroid, mapping = aes(x0 = .data$meanx, y0 = .data$meany, a = .data$a, b = .data$b, angle = .data$angle, col = .data$label), inherit.aes = FALSE) +
       ggplot2::theme_bw() +
-      ggplot2::coord_fixed(ratio = 1)
+      ggplot2::coord_fixed(ratio = 1) +
+      ggplot2::guides(color = "none")
     
     return(ggsclass)
   }
